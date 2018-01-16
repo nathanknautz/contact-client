b847b9bea59df2385b70838b4757380702375ec4
nathanknautz <nate.knautz@gmail.com>
2018-01-15 21:30:04 -0600

    updates from class

--- runner.rb
+++ runner.rb
@@ -1,6 +1,19 @@
 require 'unirest'
 
-response = Unirest.get("http://localhost:3000/one_contact_url")
+url = "http://localhost:3000/"
 
+puts "Choose an option:"
+puts "     [1] Show one contact"
+puts "     [2] Show all contacts"
+
+input_option = gets.chomp
+
+if input_option == "1"
+  url += "one_contact_url"
+else
+  url += "all_contacts"
+end
+
+response = Unirest.get(url)
 contacts = response.body
 puts JSON.pretty_generate(contacts)
